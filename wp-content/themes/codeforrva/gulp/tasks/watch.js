17095462503294ab80cc5670a0ff6a02969c2433
adamhake <adamhake@gmail.com>
2015-07-03 01:00:27 -0400

    first round of single post page styling.

--- wp-content/themes/codeforrva/gulp/tasks/watch.js
+++ wp-content/themes/codeforrva/gulp/tasks/watch.js
@@ -1,14 +1,8 @@
 var gulp       = require('gulp');
 
 gulp.task('watch', function() {
-	var server = livereload();
-
-	var reload = function(file) {
-		server.changed(file.path);
-	};
-
 	gulp.watch('app/js/**/*.js', ['browserify-app']);
-	gulp.watch('app/scss/**/*', ['sass-dev', 'autoprefixer']);
+	gulp.watch('app/scss/**/*', ['sass', 'autoprefixer']);
 	gulp.watch('app/img/**/*', ['images']);
 	gulp.watch('app/video/**/*', ['videos']);
 });
---
+++ wp-content/themes/codeforrva/single.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<article class="post container">
+<?php  while ( have_posts() ) : the_post(); ?>
+<header class="col-md-12">
+  <h1 class="post__title"><?php the_title(); ?></h1>
+  <p class="post__byline">Posted by <?php the_author_posts_link();?> on <?php the_time('F jS, Y'); ?> </p>
+</header>
+    <div class="col-md-12 post__content">
+    <?php print the_content(); ?>
+    </div>
+</article>
+<?php endwhile; ?>
+<?php get_footer(); ?>
