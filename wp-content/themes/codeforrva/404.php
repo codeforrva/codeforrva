57d7c98ca65be3a5cbf1a2e3d80d8c78602cadfa
adamhake <adamhake@gmail.com>
2015-08-12 20:40:09 -0400

    adds 404 page.

---
+++ wp-content/themes/codeforrva/404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<article class="page container">
+  <div class="row">
+  <header class="col-md-12"><h1 class="single-header">Doh! No Page here.</h1></header>
+  </div>
+  <h2><a href="/">Back to safety.</a></h2>
+</article>
+<?php get_footer(); ?>
